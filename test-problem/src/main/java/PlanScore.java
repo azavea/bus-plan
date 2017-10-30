159ef4fe10c7f96597165fea3a2e5c120b2b1e2a
James W. McClain <jmcclain@azavea.com>
2017-10-30 09:53:10 -0400

    Better Cost Function

--- test-problem/src/main/java/PlanScore.java
+++ test-problem/src/main/java/PlanScore.java
@@ -10,9 +10,9 @@ import java.util.HashMap;
 
 public class PlanScore implements EasyScoreCalculator<Plan> {
 
-    // public static final int costPerBusFixed = 10000;
-    // public static final int costPerUnitDistance = 1000;
-    // public static final double bellTime = 2.0; // in units of distance
+    public static final int costPerBusFixed = 10000;
+    public static final int costPerUnitDistance = 1000;
+    public static final double bellTime = 2.0; // in units of distance
 
     @Override
     public HardSoftScore calculateScore(Plan solution) {
@@ -20,7 +20,8 @@ public class PlanScore implements EasyScoreCalculator<Plan> {
     }
 
     public HardSoftScore calculateScore(Plan solution, Boolean verbose) {
-	int covered = 0;
+	int dollars = 0;
+	int delivered = 0;
 	java.util.HashSet<Bus> seen = new java.util.HashSet<Bus>();
 
 	if (verbose) solution.display();
@@ -31,7 +32,51 @@ public class PlanScore implements EasyScoreCalculator<Plan> {
 	    }
 	}
 
-	return HardSoftScore.valueOf(seen.size() - solution.getWeight(), 0);
+	for (Bus bus : solution.getBusList()) {
+	    SourceOrSinkOrAnchor current;
+	    HashMap<Long, Integer> kids = new HashMap();
+	    
+	    if (bus.getNext() != null) {
+		dollars += costPerBusFixed;
+		current = bus;
+		while (current != null) {
+		    SourceOrSink next = current.getNext();
+
+		    // Stop
+		    if (current instanceof Stop) {
+			Stop stop = (Stop)current;
+			long key = new Long(stop.getDestination().getNode().getUuid());
+			int totalWeight = 0;
+
+			// Total kids at this stop
+			for (int w : stop.getNode().getWeights()) {
+			    totalWeight += w;
+			}
+
+			// Add kids
+			if (kids.containsKey(key))
+			    kids.put(key, kids.get(key) + totalWeight);
+			else
+			    kids.put(key, totalWeight);
+		    }
+		    else if (current instanceof School) {
+			School school = (School)current;
+			long key = new Long(school.getNode().getUuid());
+
+			// Subtract kids
+			if (kids.containsKey(key))
+			    delivered += kids.get(key);
+			kids.remove(key);
+		    }
+
+		    if (next != null)
+			dollars += (int)(costPerUnitDistance * current.getNode().surfaceDistance(next.getNode()));
+		    current = next;
+		}
+	    }
+	}
+
+	return HardSoftScore.valueOf(delivered - solution.getWeight(), -dollars);
     }
 
 }
