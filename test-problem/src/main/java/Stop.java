788a2c861ff470321c34925d3171caaa8be2a72d
James W. McClain <jmcclain@azavea.com>
2017-11-06 12:13:59 -0500

    Reinstate Stop

---
+++ test-problem/src/main/java/Stop.java
@@ -0,0 +1,30 @@
+package com.example;
+
+import org.optaplanner.core.api.domain.entity.PlanningEntity;
+import org.optaplanner.core.api.domain.variable.InverseRelationShadowVariable;
+
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Random;
+
+import com.example.Student;
+import com.example.School;
+
+
+@PlanningEntity
+public class Stop extends SourceOrSink {
+
+    private List<Student> studentList = new ArrayList<Student>();
+
+    public Stop() {}
+
+    public Stop(Node node) { this.node = node; }
+
+    @InverseRelationShadowVariable(sourceVariableName = "stop")
+    public List<Student> getStudentList() { return this.studentList; }
+    public void setStudentList(List<Student> studentList) { this.studentList = new ArrayList<Student>(studentList); }
+
+    public String toString() {
+	return "METASOURCE" + this.node.toString() + "(" + studentList.size() + ")";
+    }
+}
