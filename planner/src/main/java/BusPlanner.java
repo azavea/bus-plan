fedc8df81f8d0685da1f817f65c93b4149da81d7
James McClain <jmcclain@azavea.com>
2017-12-08 12:18:59 -0500

    Take Garage Information From Data File

--- planner/src/main/java/BusPlanner.java
+++ planner/src/main/java/BusPlanner.java
@@ -15,22 +15,22 @@ public class BusPlanner {
         // Parameters
         String costMatrixCsv = args[0];
         String studentCsv = args[1];
-        String studentStopsCsv = null; // args[2]
-        String routeCsv = null; // args[3]
-        String assignmentCsv = null; // args[4]
+        String studentStopCsv = null;   // args[2]
+        String garageCountCsv = null;   // args[3]
+        String routeOutputCsv = null;   // args[4]
+        String studentOutputCsv = null; // args[5]
         double sigmas = 1;
         double sigmaOverMu = 1.05;
-        int busesPerGarage = 1;
         int costPerBus = 0;
         int maxRideMinutes = 90;
         int secondsPerStudent = 15;
         int studentsPerBus = 62;
 
-        if (args.length > 2) studentStopsCsv = args[2];
-        if (args.length > 3) routeCsv = args[3];
-        if (args.length > 4) assignmentCsv = args[4];
+        if (args.length > 2) studentStopCsv = args[2];
+        if (args.length > 3) garageCountCsv = args[3];
+        if (args.length > 4) routeOutputCsv = args[4];
+        if (args.length > 5) studentOutputCsv = args[5];
 
-        Plan.BUSES_PER_GARAGE = busesPerGarage;
         Plan.COST_PER_BUS = costPerBus;
         Plan.MAX_RIDE_MINUTES = maxRideMinutes;
         Plan.SECONDS_PER_STUDENT = secondsPerStudent;
@@ -40,11 +40,12 @@ public class BusPlanner {
 
         SolverFactory<Plan> solverFactory = SolverFactory.createFromXmlResource("solver.xml");
         Solver<Plan> solver = solverFactory.buildSolver();
-        Plan before = new Plan(costMatrixCsv, studentCsv, studentStopsCsv);
+        Plan before = new Plan(costMatrixCsv, studentCsv, studentStopCsv, garageCountCsv);
         Plan after = solver.solve(before);
 
         after.display();
-	if (routeCsv != null && assignmentCsv != null) after.render(routeCsv, assignmentCsv);
+	if (routeOutputCsv != null && studentOutputCsv != null)
+	    after.render(routeOutputCsv, studentOutputCsv);
         System.out.println(after.getScore());
     }
 
