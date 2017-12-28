c26b41602060b7d00c98580cb74016d0bfeb4a1f
simonkassel <kassel.simon@gmail.com>
2017-12-27 19:17:54 -0500

    update car speeds

--- open-trip-planner/src/main/scala/com/azavea/busplan/routing/BusPlanWayPropertySetNoStudents.scala
+++ open-trip-planner/src/main/scala/com/azavea/busplan/routing/BusPlanWayPropertySetNoStudents.scala
@@ -5,15 +5,12 @@ import org.opentripplanner.graph_builder.module.osm.WayPropertySet
 import org.opentripplanner.graph_builder.module.osm.WayPropertySetSource
 import org.opentripplanner.routing.edgetype.StreetTraversalPermission
 
-class BusPlanWayPropertySet() extends WayPropertySetSource {
+class BusPlanWayPropertySetNoStudents() extends WayPropertySetSource {
 
   /* (non-Javadoc)
 	 * @see org.opentripplanner.graph_builder.module.osm.WayPropertySetSource#populateProperties(org.opentripplanner.graph_builder.module.osm.WayPropertySet)
 	 */
   override def populateProperties(props: WayPropertySet): Unit = {
-
-    // Restrict cars from traversing limited access highways
-    props.setProperties("highway=motorway", StreetTraversalPermission.NONE, 2.06, 2.06)
 
     // Reduce speeds for each road type by 15%
     // Speeds ammended from source: https://github.com/opentripplanner/OpenTripPlanner/blob/bb29688dcee3e17032c57c21e81bb68362028b93/src/main/java/org/opentripplanner/graph_builder/module/osm/DefaultWayPropertySetSource.java
