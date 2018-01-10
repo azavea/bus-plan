aeea2b576b7f3877f29caad33052bbdc519cd14e
simonkassel <kassel.simon@gmail.com>
2018-01-10 18:13:51 -0500

    refactor analysis scripts

--- analysis/plan_analysis.py
+++ analysis/plan_analysis.py
@@ -14,13 +14,41 @@ import student_ride_times as srt
 
 
 class BusPlan():
+    """ One set of bus routes
+
+    Summary information for a comprehensive bus plan
+
+    Attributes:
+        route_csv: path to the router output csv
+        stop_assignment_csv: path to student assignment (solver output) csv
+        total_students: total number of students being routed in the plan
+        walk_threshold: the max distance students are allowed to walk in this
+            scenario
+        ride_times_df: Pandas DataFrame with ride time for each student
+        ride_times: array of student ride times
+        plan: Pandas df with full router output
+        routed_students: total number of students routed under this plan
+        maximum_ride_time: the maximum ride time (seconds) among all students
+        bus_times: dict of route ids and associated total drive times
+        pal: color palette (dict) with a fo color each route
+        garages: router output subset for segments starting at a garage
+        schools: router output subset for segments ending at a schools
+        stops: router output subset for segments starting at a stop
+        bus_metrics: dict of summary statistics about the route
+        student_metrics: dict of summary statistics about students
+    """
+
     def __init__(self, routes, stop_assignment, total_students=1056):
+        """Create BusPlan from router output and student assignment output"""
+        # parameters
         self.route_csv = routes
         self.stop_assignment_csv = stop_assignment
         self.total_students = total_students
         self.walk_threshold = self.get_walk_threshold()
 
-        self.ride_times_df = srt.get_student_ride_times(routes, stop_assignment)
+        # datasets
+        self.ride_times_df = srt.get_student_ride_times(routes,
+                                                        stop_assignment)
         self.ride_times = np.round(pd.to_numeric(
             self.ride_times_df['duration']) / 60)
         self.plan = ms.get_csv(routes)
@@ -28,6 +56,7 @@ class BusPlan():
         self.max_ride_times = self.ride_times_df.sort_values('duration').groupby(
             ['route_id']).last().reset_index()[['route_id', 'duration']]
         self.bus_times = self.get_route_times()
+        self.pal = ms.get_palette(self.plan)
 
         # subsets
         self.garages = self.plan[self.plan['origin_type'] == 'garage']
@@ -38,33 +67,34 @@ class BusPlan():
         self.bus_metrics = self.get_bus_metrics()
         self.student_metrics = self.get_student_metrics()
 
-    # get school bus/route related metrics
     def get_bus_metrics(self):
+        """get school bus/route related metrics"""
         durations = list(self.bus_times.values())
         return {
             'Total buses': self.plan['route_id'].nunique(),
-            'Total bus stops': self.stops.groupby(['route_id', 'origin_id']).ngroups,
+            'Total bus stops': self.stops.groupby(
+                ['route_id', 'origin_id']).ngroups,
             'Unique bus stops': self.stops['origin_id'].nunique(),
             'Garages': self.garages['origin_id'].nunique(),
             'Total active bus time (hours)': sum(durations) / 60,
             'Average route time (minutes)': np.mean(durations)
         }
 
-    # get route times for all students
     def get_route_times(self):
+        """get route times for all students"""
         route_durations = {}
         for route in self.plan['route_id'].unique():
             time = self.plan[self.plan['route_id'] == route]['time']
             route_durations[route] = (time.max() - time.min()) / 60
         return route_durations
 
-    # get a count of all students included in this plan
     def get_routed_students(self):
+        """get a count of all students included in this plan"""
         with open(self.stop_assignment_csv) as f:
             return sum([len(line.split(',')) - 1 for line in f])
 
-    # calculate student related metrics
     def get_student_metrics(self):
+        """calculate student related metrics"""
         times = np.round(self.ride_times.describe())
         return {
             'Students left behind': self.total_students - self.routed_students,
@@ -74,8 +104,8 @@ class BusPlan():
             'Maximum ride time': times['max']
         }
 
-    # get walk distance walk distance maximum from directory name
     def get_walk_threshold(self):
+        """get walk distance walk distance maximum from directory name"""
         head, tail = os.path.split(os.path.split(self.route_csv)[0])
         s = tail[-9:-6]
         try:
@@ -83,12 +113,16 @@ class BusPlan():
         except ValueError:
             return 'existing plan'
 
-    # output static map of plan
-    def map_plan(self):
-        return ms.map(self.route_csv)
+    def map(self):
+        """output static map of plan"""
+        return ms.static_map(self.plan, self.pal)
 
-    # density plot of student ride times
+    def animate(self):
+        """output animation of plan"""
+        return ms.animate(self.plan, self.pal)
+
     def plot_student_ride_time(self):
+        """density plot of student ride times"""
         sns.set_style(('whitegrid'))
         plt.figure(figsize=(10, 4))
         dens = sns.kdeplot(self.ride_times, shade=True, color="b")
@@ -99,9 +133,9 @@ class BusPlan():
 
 
 def get_all_plans(directory):
-    '''
+    """
     Create a dict with a plan for each run
-    '''
+    """
     runs = os.listdir(directory)
     bus_plans = {}
     for r in runs:
@@ -118,10 +152,13 @@ def get_all_plans(directory):
 
 
 def scheduled_ride_times(student_file, output_file=None):
-    '''
+    """
     Get existing scheduled ride times from student file
-        -input: chester-students-with-cm-cm-reference.csv
-    '''
+
+    Args:
+        student_file: Student datset (csv)
+        output_file: output csv of scheduled ride times
+    """
     students = pd.read_csv(student_file)
 
     def get_time(s):
@@ -133,53 +170,58 @@ def scheduled_ride_times(student_file, output_file=None):
     students['scheduled_ride_time'] = (
         students['drop_off'] - students['pick_up']).dt.total_seconds()
 
-    # some records are erroneous, show a pick up time of 1AM, remove those records
+    # some records are erroneous, show a pick up time of 1AM, remove those
+    # records
     students = students[students['scheduled_ride_time'] > 0]
     # select appropriate fields, rename columns
-    students = students[['compass_id', 'Run.Trip.Number', 'stop_id_cm_reference',
-                         'scheduled_ride_time']]
+    students = students[['compass_id', 'Run.Trip.Number',
+                         'stop_id_cm_reference', 'scheduled_ride_time']]
     students = students.rename(columns={'Run.Trip.Number': 'route'})
     # to match the otherdatasets
     students['compass_id'] = 'student_' + students['compass_id']
     students['stop_id_cm_reference'] = 'stop_' + \
         students['stop_id_cm_reference'].astype(str)
 
-    if output_file != None:
+    if output_file is not None:
         students.to_csv(output_file, index_label=False)
     else:
         return students
 
 
 def summary_table(proposed_plans, existing_plan):
-    '''
+    """
     Create a summary table data frame
-    '''
+    """
     def get_results_for_plan(plan, scenario=None):
         sm = pd.DataFrame.from_dict(plan.student_metrics, 'index').transpose()
         bm = pd.DataFrame.from_dict(plan.bus_metrics, 'index').transpose()
         r = pd.concat([sm, bm], 1, ignore_index=True)
-        if scenario == None:
+        if scenario is None:
             r['scenario'] = plan.walk_threshold
         else:
             r['scenario'] = scenario
         return r
     results = get_results_for_plan(existing_plan, 'existing')
     for k, v in proposed_plans.items():
-        results = pd.concat([results, get_results_for_plan(v)], 0, ignore_index=True)
+        results = pd.concat([results, get_results_for_plan(v)], 0,
+                            ignore_index=True)
     results.columns = list(existing_plan.student_metrics.keys()) + \
         list(existing_plan.bus_metrics.keys()) + ['scenario']
     return results
 
 
 def stop_eligibility_counts(eligibility_file):
+    """
+    Get array eligible stop counts for all students in the dataset
+    """
     with open(eligibility_file) as f:
         return np.array([(len(line.split(',')) - 1) for line in f])
 
 
 def comparative_ride_times_plot(bus_plans, selections, existing_plan):
-    '''
+    """
     Create a density plot with ride time density for a number of bus bus_plans
-    '''
+    """
     fig, ax = plt.subplots(figsize=(10, 5))
     colors = {'0.25 mi': '#6497b1', '0.4 mi': '#005b96', '0.5 mi': '#03396c',
               '1.0 / 0.5 mi': '#011f4b', 'existing': '#CD0000'}
@@ -196,10 +238,10 @@ def comparative_ride_times_plot(bus_plans, selections, existing_plan):
 
 
 def summary_stats_bar_plots(bus_plans, existing_plan):
-    '''
+    """
     Create a set of facetted bar plots showing the differences in performance
     metrics among all different scenarios
-    '''
+    """
     st = summary_table(bus_plans, existing_plan)
     mn = st.groupby('scenario').mean()
     mn = mn.drop(['Students left behind', 'Garages',
@@ -209,12 +251,12 @@ def summary_stats_bar_plots(bus_plans, existing_plan):
 
 
 def student_stop_eligibility_plots(input_directory):
-    '''
+    """
     Create a distribution plot of the number of stop options for
-    '''
-    f = [stop_eligibility_counts(os.path.join(input_directory,
-                                              'student-stop-eligibility-{}.csv'.format(s)))
-         for s in ['25', '40', '50', '100']]
+    """
+    f = [stop_eligibility_counts(os.path.join(
+        input_directory, 'student-stop-eligibility-{}.csv'.format(s)))
+        for s in ['25', '40', '50', '100']]
     fig, ax = plt.subplots(figsize=(12, 7))
     colors = {'0.25 mi': 'green', '0.4 mi': 'blue',
               '0.5 mi': 'red', '1.0 / 0.5 mi': 'black'}
