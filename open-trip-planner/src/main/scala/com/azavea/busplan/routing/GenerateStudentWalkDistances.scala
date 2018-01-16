f62107755f127cda89d22d3ea230cd04922213fb
simonkassel <kassel.simon@gmail.com>
2018-01-16 12:19:06 -0500

    initialize main class

---
+++ open-trip-planner/src/main/scala/com/azavea/busplan/routing/GenerateStudentWalkDistances.scala
@@ -0,0 +1,12 @@
+package com.azavea.busplan.routing
+
+import org.apache.commons.csv.CSVFormat;
+import org.apache.commons.csv.CSVRecord;
+
+import java.io._
+import scala.collection.JavaConverters._
+
+/** Measure walk distances for students to their stop within a plan */
+object GenerateStudentWalkDistances {
+
+}
